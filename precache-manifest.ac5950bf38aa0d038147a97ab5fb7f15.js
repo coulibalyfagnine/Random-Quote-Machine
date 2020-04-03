7e8b6fbd56b8618a9eab6180b41b6cd13b6729b6
coulibalyfagnine <coulibalyfagnine@gmail.com>
2020-04-03 22:38:42 +0200

    Updates

---
+++ precache-manifest.ac5950bf38aa0d038147a97ab5fb7f15.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5a9f352d810b617b44ed168f0c063ed",
+    "url": "/Random-Quote-Machine/index.html"
+  },
+  {
+    "revision": "b3e4904f2cba1be0e3a3",
+    "url": "/Random-Quote-Machine/static/css/2.07548e1c.chunk.css"
+  },
+  {
+    "revision": "03ff900674af5cdbd300",
+    "url": "/Random-Quote-Machine/static/css/main.35ebe003.chunk.css"
+  },
+  {
+    "revision": "b3e4904f2cba1be0e3a3",
+    "url": "/Random-Quote-Machine/static/js/2.14f5c656.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Random-Quote-Machine/static/js/2.14f5c656.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03ff900674af5cdbd300",
+    "url": "/Random-Quote-Machine/static/js/main.32167321.chunk.js"
+  },
+  {
+    "revision": "b6af4d0bb5afe18f3e75",
+    "url": "/Random-Quote-Machine/static/js/runtime-main.92500b7b.js"
+  }
+]);
